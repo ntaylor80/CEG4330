911656e879e6d7a2f9d0b2a0d76aea533ae1d61b
Noah <noah9990@yahoo.com>
2018-01-23 16:36:06 -0500

    pitches

---
+++ Lab 1/Pitch/Pitch.ino
@@ -0,0 +1,51 @@
+/* @file HelloKeypad.pde
+|| @version 1.0
+|| @author Alexander Brevig
+|| @contact alexanderbrevig@gmail.com
+||
+|| @description
+|| | Demonstrates the simplest use of the matrix Keypad library.
+|| #
+*/
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+int note;
+int pitch;
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+byte rowPins[ROWS] = {9, 2, 3, 5}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {4, 6, 7, 8}; //connect to the column pinouts of the keypad
+byte speaker = 11;
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup(){
+  Serial.begin(9600);
+}
+  
+void loop(){
+  char key = keypad.getKey();
+  if (key){
+    if (key <= '7' && key >= '1'){
+       note = key - '0';
+       
+       pitch = 261.2 * pow(2.0,(note/12.0));
+       tone(speaker, pitch);
+       Serial.println(pitch);
+    }
+     
+  }else{
+    noTone(speaker);
+  }
+  
+  delay(100);
+  
+}
+
