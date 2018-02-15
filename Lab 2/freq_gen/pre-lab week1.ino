063801fbd4890a91b49e4a25fa4188bdeb0d44a9
Jason P Elting <JasonElting@users.noreply.github.com>
2018-02-15 13:56:53 -0500

    Update pre-lab week1.ino

--- Lab 2/freq_gen/pre-lab week1.ino
+++ Lab 2/freq_gen/pre-lab week1.ino
@@ -1,4 +1,10 @@
 /* Jason Elting & Noah Taylor
+
+(1)(Week 1: 10%) Write the code for Experiment (1).  Identify timer 2 registers that you will 
+use for Experiment (2) and explain how to control the frequency and duty cycle by using 
+those registers.
+
+
 Registers TCCR2B and TCCR2A will be used to control the frequency,
 by changing their value and the prescalar to meet the closest frequency possible.
 
