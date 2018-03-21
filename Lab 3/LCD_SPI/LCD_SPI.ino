3f929798b8a85602e5b08832695e265bbffb601a
unknown <jason.elting@gmail.com>
2018-03-21 11:18:31 -0400

    added slave mode

--- Lab 3/LCD_SPI/LCD_SPI.ino
+++ Lab 3/LCD_SPI/LCD_SPI.ino
@@ -50,15 +50,35 @@ void lcdSPISetup();
 
 //bluetooth setup code
 void bluetoothSetup(){
-   Serial.begin(115200);  // Begin the serial monitor at 9600bps
-
-  bluetooth.begin(115200);  // The Bluetooth Mate defaults to 115200bps
+  bluetooth.begin(115200);
+  Serial.begin(115200);
+   
   bluetooth.print("$");  // Print three times individually
   bluetooth.print("$");
   bluetooth.print("$");  // Enter command mode
   delay(100);  // Short delay, wait for the Mate to send back CMD
   bluetooth.println("SN,NJ");  // Temporarily Change the baudrate to 9600, no parity
   // 115200 can be too fast at times for NewSoftSerial to relay the data reliably
+  
+//    delay(100);
+//  //sets up baud
+//  bluetooth.print("$");  // Print three times individually
+//  bluetooth.print("$");
+//  bluetooth.print("$");  // Enter command mode
+//  delay(100);
+//  bluetooth.print("U,9600");
+//  
+
+  delay(100);
+  //sets up slavery
+  bluetooth.print("$");  // Print three times individually
+  bluetooth.print("$");
+  bluetooth.print("$");  // Enter command mode
+  delay(100);
+  bluetooth.print("SM,0");
+
+
+  
 }
 
 // Send character to LCD over SPI. Software implementation for slower SCLK
